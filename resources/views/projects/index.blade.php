bf51da4934cb120e97e096b89231568b9b57fd8f
vagrant <tomas@forumcinemashe.lt>
2019-04-05 15:59:08 +0000

    Describe Tailwind class in index.blade.php to style projects page.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,15 +3,19 @@
 @section('content')
 
 <div class="flex items-center mb-3">
-	<h1 class="mr-auto">Birdboard</h1>
 	<a href="/projects/create">Create New Project</a>
 </div>
-	<ul>
-		@foreach ($projects as $project)
-			<li>
-				<a href="{{ $project->path() }}">{{ $project->title }}</a>
-			</li>
-		@endforeach
-	</ul>
+
+<div class="flex">
+	@forelse ($projects as $project)
+		<div class="bg-white p-5 mr-4 rounded shadow w-1/3" style="height: 200px">
+			<h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+
+			<div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+		</div>
+	@empty
+		<div>No projects yet.</div>
+	@endforelse
+</div>
 
 @endsection
