62f336f6623a395891e4e815fc698521eb5d9f6b
vagrant <tomas@forumcinemashe.lt>
2019-04-06 18:28:22 +0000

    Style single project page adapting Tailwind CSS classes.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
+<header class="flex items-center mb-3 py-4">
+	<div class="flex justify-between items-end w-full">
+		<p class="text-grey text-sm font-normal">
+			<a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+		</p>
+		<a href="/projects/create" class="button">New Project</a>
+	</div>
+</header>
 
-	<h1>{{ $project->title }}</h1>
-	<div>{{ $project->description }}</div>
-	<a href="/projects">Go back</a>
+<main>
+	<div class="lg:flex -mx-3">
+		<div class="lg:w-3/4 px-3 mb-6">
+			<div class="mb-8">
+				<h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+				<div class="card">Lorem ipsum</div>
+			</div>
+			<div>
+				<h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+				<textarea class="card w-full" style="min-height: 180px">Lorem ipsum</textarea> 
+			</div>
+		</div>
+		<div class="lg:w-1/4 px-3">
+			@include ('projects.card')
+		</div>
+	</div>
+</main>
+
 
 @endsection
