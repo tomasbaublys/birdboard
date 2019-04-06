318b6daa38f5385fe9542420e913d2aec8fbca03
vagrant <tomas@forumcinemashe.lt>
2019-04-06 14:02:38 +0000

    Continue working on the CSS for the projects dashboard.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,20 +2,27 @@
 
 @section('content')
 
-<div class="flex items-center mb-3">
-	<a href="/projects/create">Create New Project</a>
-</div>
+<header class="flex items-center mb-3 py-4">
+	<div class="flex justify-between items-center w-full">
+		<h2 class="text-grey text-sm font-normal">My Projects</h2>
+		<a href="/projects/create" class="button">New Project</a>
+	</div>
+</header>
 
-<div class="flex">
+<main class="lg:flex lg:flex-wrap -mx-3">
 	@forelse ($projects as $project)
-		<div class="bg-white p-5 mr-4 rounded shadow w-1/3" style="height: 200px">
-			<h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+		<div class="lg:w-1/3 px-3 pb-6">
+			<div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
+				<h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue pl-4">
+					<a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+				</h3>
 
-			<div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+				<div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+			</div>
 		</div>
 	@empty
 		<div>No projects yet.</div>
 	@endforelse
-</div>
+</main>
 
 @endsection
--- tailwind.js
+++ tailwind.js
@@ -99,7 +99,7 @@ let colors = {
   'blue-darkest': '#12283a',
   'blue-darker': '#1c3d5a',
   'blue-dark': '#2779bd',
-  'blue': '#3490dc',
+  'blue': '#47cdff',
   'blue-light': '#6cb2eb',
   'blue-lighter': '#bcdefa',
   'blue-lightest': '#eff8ff',
