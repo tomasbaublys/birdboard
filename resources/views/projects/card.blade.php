d89d5fafa85066ee35785e397feb6a2f1ef72122
vagrant <tomas@forumcinemashe.lt>
2019-04-29 14:52:16 +0000

    Prohibit for any invited user to delete the project entirely.

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -3,11 +3,14 @@
 		<a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
 	</h3>
 	<div class="text-grey mb-4 flex-1">{{ str_limit($project->description, 100) }}</div>
-	<footer>
-		<form method="POST" action="{{ $project->path() }}" class="text-right">
-			@method('DELETE')
-			@csrf
-			<button type="submit" class="text-xs">Delete</button>
-		</form>
-	</footer>
+
+	@can ('manage', $project)
+		<footer>
+			<form method="POST" action="{{ $project->path() }}" class="text-right">
+				@method('DELETE')
+				@csrf
+					<button type="submit" class="text-xs">Delete</button>
+			</form>
+		</footer>
+	@endcan
 </div>
