ee02885ef6638671edc1a57fb9ad52924c508bd2
vagrant <tomas@forumcinemashe.lt>
2019-04-30 11:56:24 +0000

    Create dark theme with tailwind css variables.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <header class="flex items-center mb-3 pb-4">
 	<div class="flex justify-between items-end w-full">
-		<p class="text-grey text-sm font-normal">
-			<a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+		<p class="text-default text-sm font-normal">
+			<a href="/projects" class="text-default text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
 		</p>
 		 <div class="flex items-center">
             @foreach ($project->members as $member)
@@ -28,7 +28,7 @@
 	<div class="lg:flex -mx-3">
 		<div class="lg:w-3/4 px-3 mb-6">
 			<div class="mb-8">
-				<h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+				<h2 class="text-lg text-default font-normal mb-3">Tasks</h2>
 
 				@foreach ($project->tasks as $task)
 					<div class="card mb-3">
@@ -36,7 +36,7 @@
 						@method('PATCH')
 						@csrf
 						<div class="flex">
-							<input name="body" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-grey' : '' }}">
+							<input name="body" value="{{ $task->body }}" class="bg-card text-default w-full {{ $task->completed ? 'text-default' : '' }}">
 							<input type="checkbox" name="completed" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
 						</div>
 						</form>
@@ -47,12 +47,12 @@
 					<form action="{{ $project->path() . '/tasks' }}" method="post">
 						@csrf
 
-						<input placeholder="Begin adding tasks..." class="w-full" name="body">
+						<input placeholder="Begin adding tasks..." class="bg-card text-default w-full" name="body">
 					</form>
 				</div>
 			</div>
 			<div> 
-				<h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+				<h2 class="text-lg text-default font-normal mb-3">General Notes</h2>
 				<form method="post" action="{{ $project->path() }}">
 					@method('PATCH')
 					@csrf
