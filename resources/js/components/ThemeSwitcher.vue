569c18e28e1035a355ef7326a5e720f2c6ad49b9
vagrant <tomas@forumcinemashe.lt>
2019-04-30 17:04:04 +0000

    Add vue javascript functionality to switch between themes.

---
+++ resources/js/components/ThemeSwitcher.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex items-center mr-8">
+        <button class="rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none" 
+            :class="{ 'border-accent': selectedTheme == 'theme-light' }"
+            style="background: #f5f6f9" 
+            @click="selectedTheme = 'theme-light'">
+        </button>
+        <button class="rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none" 
+            :class="{ 'border-accent': selectedTheme == 'theme-dark' }"
+            style="background: #222" 
+            @click="selectedTheme = 'theme-dark'">
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+       data() {
+            return {
+                selectedTheme: 'theme-light'
+            };
+        },
+
+        watch: {
+            selectedTheme() {
+                document.body.className = document.body.className.replace(/theme-\w+/, this.selectedTheme);
+            }
+        }
+    }
+</script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="theme-dark bg-page">
+<body class="theme-light bg-page">
     <div id="app">
         <nav class="bg-header">
             <div class="container mx-auto">
@@ -40,37 +40,32 @@
 
                     <div>
                         <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
+                        <div class="flex items-center ml-auto">
                             <!-- Authentication Links -->
                             @guest
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 @endif
                             @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                <theme-switcher></theme-switcher>  
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
                                     </a>
 
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
                             @endguest
-                        </ul>
+                        </div>
                     </div>
                 </div>
             </div>
