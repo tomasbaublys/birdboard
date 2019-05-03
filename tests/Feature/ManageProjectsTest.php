bb5b1c001087359c73ed7e5472cc59d0dc37d3a4
vagrant <tomas@forumcinemashe.lt>
2019-05-03 09:16:23 +0000

    Allow tasks to be created alongside the project, corrected ProjectController.php store method.

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -38,11 +38,29 @@ class ManageProjectsTest extends TestCase
 
         $this->get('/projects/create')->assertStatus(200);
   
-        $this->followingRedirects()->post('/projects', $attributes = factory(Project::class)->raw())
+        $this->followingRedirects()
+            ->post('/projects', $attributes = factory(Project::class)->raw())
             ->assertSee($attributes['title'])
             ->assertSee($attributes['description'])
             ->assertSee($attributes['notes']);
     }
+    /** @test */ 
+    public function tasks_can_be_included_as_part_a_new_project_creation()
+    {
+        $this->signIn();
+
+        $attributes = factory(Project::class)->raw();
+
+        $attributes['tasks'] = [
+            ['body' => 'Task 1'],
+            ['body' => 'Task 2']
+        ];
+
+        $this->post('/projects', $attributes);
+
+        $this->assertCount(2, Project::first()->tasks);
+    }
+
 
     /** @test */
     public function a_user_can_see_all_projects_they_have_been_invited_to_on_their_dashboard()
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,6 @@ require('laravel-mix-tailwind');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .tailwind();
+   .tailwind().options({
+      processCssUrls: false
+   });
